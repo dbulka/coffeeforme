9f21f7b9689828a95aa8e544c681d28008bafe5a
dbulka <dzm.bulka@gmail.com>
2019-03-12 23:29:08 +0300

    point 4 was done

--- core/interfaces/sale_interface/sale_interface.py
+++ core/interfaces/sale_interface/sale_interface.py
@@ -15,7 +15,7 @@ class SaleInterface(object):
         beverage_type_list = SaleController().get_beverage_types_list()
         type_numbers = []
         print("""
-              Please choose your beverage type:
+              Please choose beverage type:
               """)
         for beverage_type in beverage_type_list:
             print('#{0} {1} {2}$'.format(beverage_type[0],beverage_type[1],beverage_type[2]))
@@ -30,17 +30,51 @@ class SaleInterface(object):
         """
         resp = True
         while resp:
-            choice = input('Please enter number of beverage type #')
+            choosen_beverage = input('Please enter number of beverage type #')
             try:
-                if int(choice) in type_numbers:
+                if int(choosen_beverage) in type_numbers:
                     resp = False
                 else:
                     print('Enter number attentive!')
             except:
                 print('Enter number attentive!')
-        logging.getLogger(__name__).info('Bavarage type %s was choose ' %choice)
-        return choice
+        logging.getLogger(__name__).info('Bavarage type %s was choose ' %choosen_beverage)
+        return choosen_beverage
+
+    def show_beverage_ingredients(self):
+        """
+        show beverage ingredients
+        :return: show beverage ingredients
+        """
+        beverage_ingredients_list = SaleController().get_beverage_ingredients_list()
+        ingredients_numbers = []
+        print("""
+              Please choose additional ingredient:
+              """)
+        for ingredient in beverage_ingredients_list:
+            print('#{0} {1} {2}$'.format(ingredient[0],ingredient[1],ingredient[2]))
+        for i in range(len(beverage_ingredients_list)):
+            ingredients_numbers.append(i+1)
+        return ingredients_numbers
+
+    def choose_beverage_ingredient(self, ingredients_numbers):
+        """
+        choose beverage ingredient
+        :return: choosen beverage ingredient
+        """
+        resp = True
+        while resp:
+            choosen_ingredient = input('Please enter number of beverage ingredient #')
+            try:
+                if int(choosen_ingredient) in ingredients_numbers:
+                    resp = False
+                else:
+                    print('Enter number attentive!')
+            except:
+                print('Enter number attentive!')
+        logging.getLogger(__name__).info('Bavarage ingredient %s was choose ' %choosen_ingredient)
+        return choosen_ingredient
 
 sm = SaleInterface()
-gb = sm.show_beverage_type()
-sm.choose_beverage_type(gb)
+st = sm.show_beverage_ingredients()
+sm.choose_beverage_ingredient(st)
